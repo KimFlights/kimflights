2c9572215f9dc33bef3fb3a1b596fd539e2a5d91
TimeBudhil <71571047+TimeBudhil@users.noreply.github.com>
2026-06-02 18:37:36 -0500

    fix routing

--- kimflights/src/main/java/com/kimgroup/kimflights/KimflightsApplication.java
+++ kimflights/src/main/java/com/kimgroup/kimflights/KimflightsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KimflightsApplication {
 
 	public static void main(String[] args) {
+		//app is here
 		SpringApplication.run(KimflightsApplication.class, args);
 	}
 
