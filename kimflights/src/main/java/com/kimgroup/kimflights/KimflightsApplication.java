6a86629e66bbf11a79ae11669d69bb5734639335
TimeBudhil <71571047+TimeBudhil@users.noreply.github.com>
2026-06-02 15:43:16 -0500

    initial comit

---
+++ kimflights/src/main/java/com/kimgroup/kimflights/KimflightsApplication.java
@@ -0,0 +1,13 @@
+package com.kimgroup.kimflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimflightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimflightsApplication.class, args);
+	}
+
+}
---
+++ kimflights/src/test/java/com/kimgroup/kimflights/KimflightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimgroup.kimflights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimflightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
