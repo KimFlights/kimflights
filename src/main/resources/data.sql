e8c9ef960d3fadbc277c259fd74a71e0b55803dd
Hudson Hoang <huyhoang201555@gmail.com>
2026-06-08 14:35:30 -0500

    Update to Postgresql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO aircraft (name, manufacturer, seat_capacity) VALUES ('737 Max', 'Boeing', 189);
-INSERT INTO aircraft (name, manufacturer, seat_capacity) VALUES ('A320neo', 'Airbus', 180);
+INSERT INTO aircraft (name, manufacturer, seat_capacity) VALUES ('737 Max', 'Boeing', 189) ON CONFLICT (name) DO NOTHING;
+INSERT INTO aircraft (name, manufacturer, seat_capacity) VALUES ('A320neo', 'Airbus', 180) ON CONFLICT (name) DO NOTHING;
