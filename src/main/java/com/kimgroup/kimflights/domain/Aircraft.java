4224f0270245cae1bc72d47fe12063336061ec24
Hudson Hoang <huyhoang201555@gmail.com>
2026-06-08 14:08:13 -0500

    Update JPA H2

--- src/main/java/com/kimgroup/kimflights/domain/Aircraft.java
+++ src/main/java/com/kimgroup/kimflights/domain/Aircraft.java
@@ -1,14 +1,17 @@
 package com.kimgroup.kimflights.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Entity
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Aircraft {
+    @Id
     private String name;
     private String manufacturer;
     private Integer seatCapacity;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO aircraft (name, manufacturer, seat_capacity) VALUES ('737 Max', 'Boeing', 189);
+INSERT INTO aircraft (name, manufacturer, seat_capacity) VALUES ('A320neo', 'Airbus', 180);
