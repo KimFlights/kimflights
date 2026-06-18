3ecbaa076dada92b38e0841354d5b75bdec01269
Sharmaxz <shadowvonwolfkin@gmail.com>
2026-06-17 19:45:57 -0500

    chore: comment out app service in compose.yaml and add security mocks to AircraftControllerTest

--- src/main/java/com/kimgroup/kimflights/payment/repository/InvoiceRepository.java
+++ src/main/java/com/kimgroup/kimflights/payment/repository/InvoiceRepository.java
@@ -1,7 +1,6 @@
 package com.kimgroup.kimflights.payment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.kimgroup.kimflights.payment.model.Invoice;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
--- src/main/java/com/kimgroup/kimflights/payment/repository/TransactionRepository.java
+++ src/main/java/com/kimgroup/kimflights/payment/repository/TransactionRepository.java
@@ -2,7 +2,6 @@ package com.kimgroup.kimflights.payment.repository;
 
 import com.kimgroup.kimflights.payment.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
